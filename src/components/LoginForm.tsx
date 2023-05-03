4797a02e75b1e30a16c7d6730da26df4a81f2701
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-03 16:54:57 +0530

    Added Login and Chat

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,44 +1,46 @@
-import { useEffect } from 'react';
+import { api } from '../api';
 import { useNavigate } from 'react-router-dom';
 import './LoginForm.css';
-import {api} from "../api";
+import { useEffect } from 'react';
 
 export default function LoginForm() {
-
   const navigate = useNavigate();
+
+  useEffect(() => {
+    api.account.get().then(() => {
+      navigate('/chat?room=general');
+    }, () => {});
+  }, []);
 
   async function login(e: any) {
     e.preventDefault();
-
-    const name = e.target.name.value;
-    const room = e.target.room.value;
-
+    const form = new FormData(e.target);
+    const name = form.get('name') as string;
+    const room = form.get('room') as string;
     await api.account.createAnonymousSession();
-
     await api.account.updateName(name);
 
-    navigate('/chat?room=' + room);
-
+    navigate(`/chat?room=${room}`);
   }
 
   return (
-    <div className="login-container">
-      <span className="join-room">Join a room</span>
+      <div className="login-container">
+        <span className="join-room">Join a room</span>
 
-      <form className="login-form" onSubmit={login}>
-        <p className="login-name">
-          <label htmlFor="name">Name</label>
+        <form className="login-form" onSubmit={login}>
+          <p className="login-name">
+            <label htmlFor="name">Name</label>
 
-          <input type="text" id="name" name="name" placeholder="Enter Name" />
-        </p>
+            <input type="text" id="name" name="name" placeholder="Enter Name"/>
+          </p>
 
-        <p className="login-room">
-          <label htmlFor="room">Room</label>
-          <input type="text" id="room" name="room" placeholder="Room Name" />
-        </p>
+          <p className="login-room">
+            <label htmlFor="room">Room</label>
+            <input type="text" id="room" name="room" placeholder="Room Name"/>
+          </p>
 
-        <button type="submit">Start Chatting</button>
-      </form>
-    </div>
+          <button type="submit">Start Chatting</button>
+        </form>
+      </div>
   );
 }
